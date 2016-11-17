e62869eef2b0218988a99a4d722f06e5492f8bda
Oksana Melnik <kyoshiniimura@gmail.com>
2016-11-17 15:21:53 +0300

    Add init

---
+++ config/deploy/production.rb
@@ -0,0 +1,12 @@
+# production.rb
+set :rails_env, :production
+
+# Настраиваем ssh до сервера
+server "<--input production host-->", :app, :web, :db, :primary => true
+
+# Авторизационные данные
+set :user, "my-test-project"
+set :group, "my-test-project"
+set :password, '<--password-->'
+set :keep_releases, 5
+set :repository, 'prod'
